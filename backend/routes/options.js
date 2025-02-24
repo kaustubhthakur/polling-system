e16c4b10ff599883a507606f5780862eb6e72653
Kaustubh <kaustubhthakur76@gmail.com>
2025-02-24 22:30:57 +0530

    first commit

--- backend/routes/options.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router();
-const protectRoute = require('../utils/protectRoute')
-const {createoption} = require('../controllers/option')
-router.post('/:queryid',protectRoute,createoption)
-module.exports = router;
